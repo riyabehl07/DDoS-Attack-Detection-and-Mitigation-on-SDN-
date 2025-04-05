fb908713b78124f8a6409cdae753ba88a345f0db
riyabehl07 <155184236+riyabehl07@users.noreply.github.com>
2025-04-06 00:43:27 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,23 @@
+# CICIDS2017
+
+CICIDS2017.ipynb my old work.
+
+## my current work:
+
+**data.py:** read csv file, separate the dataset as X and y,separate the dataset as X_train,X_test,y_train and y_test.
+
+**featureSelection.py:** makes the feature selection.
+
+**model.py:** create a Model and calculate accuracy, confusion matrix, precision, recall, f1-score.
+
+**model_ANN.py:** create a ANN model and save,calculate confusion matrix. Load saved model and calculate confusion matrix.
+
+**test.py:** contains examples .py files
+
+
+------
+### Update
+
+You can reach the dataset I used in this project from the link below
+
+[dataset.csv drive url](https://drive.google.com/file/d/1fFXCmqdQcWsi7XdfSpj31k17lNZWawZW/view?usp=sharing)
---
+++ test.py
@@ -0,0 +1,45 @@
+
+import data 
+import model
+import model_ANN
+import featureSelection
+
+# In[] 
+fromPath="dataset.csv"
+LabelColumnName=' Label'
+dataset,featureList=data.loadData(fromPath,LabelColumnName,2)
+X,y=data.datasetSplit(dataset,LabelColumnName)
+X_train, X_test, y_train, y_test= data.train_test_dataset(X,y)
+
+# In[]
+fromPath="dataset.csv"
+LabelColumnName=' Label'
+dataset,featureList=data.loadData(fromPath,LabelColumnName,4)
+X,y=data.datasetSplit(dataset,LabelColumnName)
+X_train, X_test, y_train, y_test= data.train_test_dataset(dataset)
+
+# In[]
+from sklearn.tree import DecisionTreeClassifier
+decisionTree =model.Model(X_train, X_test, y_train, y_test, DecisionTreeClassifier(random_state = 0), ['Anormal','Normal'])
+decisionTree.confusionMatrix()
+decisionTree.roc()
+decisionTree.prfTable()
+
+# In[]
+
+epochCount=100
+batchSize=128
+input_dim=78
+unit1=78
+unit2=4
+unit3=4
+targetNames=['Normal','DDoS']
+fileName="78_4_ann_4_128"
+model_ANN.createANN(X_train, X_test, y_train, y_test,epochCount,batchSize,input_dim,unit1,unit2,unit3,targetNames,fileName)
+
+# In[]
+model_ANN.loadANN("78_8_ann_4_64.json","78_8_ann_4_64.h5",X_test,y_test,batchSize,targetNames)
+# In[]
+featureScoreDataFrame,featureDataFrame=featureSelection.randomForestSelection(X,y,featureList,' Label',dataset,15)
+# In[]
+featureScoreDataFrame,featureDataFrame=univariateSelection(X,y,featureList,' Label',dataset,15)
